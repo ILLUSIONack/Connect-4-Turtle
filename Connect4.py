c1143c15146fd961afb427f0772013cba7ffc62e
Illusionack <usman-ack@hotmail.com>
2018-11-09 16:20:41 +0100

    edited design/fixed winner

--- Connect4.py
+++ Connect4.py
@@ -4,18 +4,17 @@ YELLOW = 1
 RED = 2
 
 
-# Draw the grid on screen with all the tokens
+# Draw the grid with all the empty slots
 def draw_grid(grid):
-    global RED, YELLOW
     myPen.setheading(0)
     myPen.goto(-150, 130)
     for rower in range(0, 6):
         for col in range(0, 7):
             if grid[rower][col] == 0:
                 myPen.fillcolor("#FFFFFF")
-            elif grid[rower][col] == RED:
+            elif grid[rower][col] == 2:
                 myPen.fillcolor("#FF0000")
-            elif grid[rower][col] == YELLOW:
+            elif grid[rower][col] == 1:
                 myPen.fillcolor("#FFFF00")
 
             myPen.begin_fill()
@@ -33,11 +32,23 @@ def draw_grid(grid):
         myPen.setheading(0)
         myPen.getscreen().update()
 
+def drawsquare():
+    myPen.up()
+    myPen.setheading(0)
+    myPen.goto(-178, -146)
+    myPen.begin_fill()
+    for b in range(4):
+        myPen.color("blue")
+        myPen.pendown()
+        myPen.forward(355)
+        myPen.left(90)
+    myPen.up()
+    myPen.end_fill()
 
 def check_if_winner(grid, color):
     # Vertical row checking
     for r in range(6):
-        for c in range(7):
+        for c in range(3):
             if grid[r][c] == color and grid[r][c+1] == color and grid[r][c+2] == color and grid[r][c+3] == color:
                 return color
     # Horizontal row checking
@@ -45,7 +56,7 @@ def check_if_winner(grid, color):
         for y in range(7):
             if grid[x][y] == color and grid[x+1][y] == color and grid[x+2][y] == color and grid[x+3][y] == color:
                 return color
-    # Diagnol checking
+    # Diagonal checking
     for i in range(3):
         for z in range(3):
             if grid[i][z] == color and grid[i+1][z+1] == color and grid[i+2][z+2] == color and grid[i+3][z+3] == color:
@@ -57,8 +68,7 @@ myPen = turtle.Turtle()
 myPen.hideturtle()
 myPen.speed(500)
 window = turtle.Screen()
-window.bgcolor("#2288FF")
-myPen.color("#2288FF")
+window.bgcolor("white")
 myPen.speed(0)
 
 # Initialise empty 6 by 7 connect4 grid
@@ -67,28 +77,32 @@ for rows in range(0, 6):
     connect4.append([])
     for cols in range(0, 7):
         connect4[rows].append(0)
+drawsquare()
 draw_grid(connect4)
 
 # Play the game, take it in turn. Up to 42 turns
 for turn in range(1, 43):
-    column_string = window.numinput("Your turn", "Pick column number:", 0, minval=0, maxval=6)
+    column_string = window.numinput("Your turn", "Pick column number:", 0, minval=1, maxval=7)
     column = int(column_string)
-    while connect4[0][column] != 0:
+    cadd = column - 1
+    while connect4[0][cadd] != 0:
         # This column is already full, pick another one
-        column_string = window.numinput("Your turn", "Pick other column number row is full:", 0, minval=0, maxval=6)
+        column_string = window.numinput("Your turn", "Pick other column number row is full:", 0, minval=1, maxval=7)
         column = int(column_string)
+        cadd = column - 1
 
-    # Make the token slide to the bottom of the grid (Stacked on top of any other existing tokens)
+    # Make the chip slide to the bottom of the board, row starting from 5 then minus 1
     row = 5
-    while connect4[row][column] != 0:
+    while connect4[row][cadd] != 0:
         row = row - 1
 # Find out the colour of the current player (1 or 2)
     playerColor = int((turn % 2) + 1)
     # Place the token on the grid
-    connect4[row][column] = playerColor
+    connect4[row][cadd] = playerColor
     # Draw the grid
 
     winner = check_if_winner(connect4, playerColor)
+    draw_grid(connect4)
     if winner == 2:
         myPen.penup()
         myPen.color("black")
